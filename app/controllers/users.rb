5ad2d45be41587b2bf9788fe4f03ef8eaa7d81b1
Carl Sutherland <carl@humblesoftware.com>
2011-12-04 21:13:47 -0500

    Added users controller generated with 'padrino g controller users get:index get:login get:register post:login post:register'

---
+++ app/controllers/users.rb
@@ -0,0 +1,36 @@
+TenThousandHoursApp.controllers :users do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :index do
+  end
+
+  get :login do
+  end
+
+  get :register do
+  end
+
+  post :login do
+  end
+
+  post :register do
+  end
+
+end
