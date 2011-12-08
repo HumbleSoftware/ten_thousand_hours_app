8d3086b0ce6687b0872db6b4b36f547ad72239d1
Carl Sutherland <carl@humblesoftware.com>
2011-12-08 18:02:20 -0500

    Added entry relation.

--- models/entry.rb
+++ models/entry.rb
@@ -1,9 +1,10 @@
 class Entry
   include DataMapper::Resource
 
+  belongs_to :project
+
   # property <name>, <type>
   property :id, Serial
-  property :project_id, Integer
   property :category_id, Integer
   property :date, Date
   property :time, Time
