485d9be305900438ac7cfac30a82352e3556ed94
Carl Sutherland <carl@humblesoftware.com>
2011-12-08 10:58:51 -0500

    Added Admin::AccessControl helper.

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ class TenThousandHoursApp < Padrino::Application
   register Padrino::Rendering
   register Padrino::Mailer
   register Padrino::Helpers
+  register Padrino::Admin::AccessControl
 
   enable :sessions
 
