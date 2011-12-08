d431dcf68fce435144d032548714b7d79cbd0953
Carl Sutherland <carl@humblesoftware.com>
2011-12-08 16:38:02 -0500

    Implemented create method.

--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -9,6 +9,13 @@ TenThousandHoursApp.controllers :projects do
   end
 
   post :create do
+    @project = Project.new(params[:project])
+    @project.account = current_account
+    if @project.save
+      redirect url(:projects, :index)
+    else
+      render 'projects/new'
+    end
   end
 
 end
