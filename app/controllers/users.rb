f5620c947047aee3848c33f1dec2fb7736152bd4
Carl Sutherland <carl@humblesoftware.com>
2011-12-08 09:43:09 -0500

    Added user registration model integration w/ default role user and thankyou page.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -30,10 +30,24 @@ TenThousandHoursApp.controllers :users do
     render 'users/register'
   end
 
+  get :thankyou, :map => '/thankyou' do
+    render 'users/register_thankyou'
+  end
+
   post :login, :map => '/login' do
   end
 
   post :register, :map => '/register' do
+    @account = Account.new(params[:account])
+
+    # Assign default role, user
+    @account.role = 'user'
+
+    if @account.save
+      redirect url(:users, :thankyou)
+    else
+      render 'users/register'
+    end
   end
 
 end
