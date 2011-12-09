20e188faee1d314a4e67cbbd9b4f500c7ae6e77b
Carl Sutherland <carl@humblesoftware.com>
2011-12-09 13:38:01 -0500

    Added validation specs to entry.

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -5,4 +5,24 @@ describe "Entry Model" do
   it 'can be created' do
     entry.should_not be_nil
   end
+
+  it "doesn't validate" do
+    entry.valid?.should_not be_true
+  end
+
+  let(:entry) { Entry.new(:date => '10/10/2010') }
+  it "doesn't validate with only a date" do
+    entry.valid?.should_not be_true
+  end
+
+  let(:entry) { Entry.new(:time => '10/10/2010') }
+  it "doesn't validate with only a time" do
+    entry.valid?.should_not be_true
+  end
+
+  let(:entry) { Entry.new(:time => '10/10/2010', :date => '10/10/2010') }
+  it 'validates' do
+    entry.valid?.should_not be_true
+  end
+
 end
