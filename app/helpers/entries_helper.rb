d659e96a7487112dfbb5a1450c66a3ae727c077d
Carl Sutherland <carl@humblesoftware.com>
2011-12-09 18:29:52 -0500

    Added helper for formatting time.

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,7 +1,7 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 TenThousandHoursApp.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def format_time(time)
+    (time / 60).to_s + ':' + (time % 60).to_s
+  end
 end
