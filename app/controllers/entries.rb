9c03f0dc1a0fde05ed31823dda6a28e86531cf98
Carl Sutherland <carl@humblesoftware.com>
2011-12-09 15:18:33 -0500

    Add parsing for date, time fields.

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -1,10 +1,34 @@
 TenThousandHoursApp.controllers :entries, :parent => :projects do
 
   post :create do
+
+    entry = params[:entry]
+    date = entry[:date]
+    time = entry[:time]
+
+    @entry = Entry.new
     @project = Project.get(params[:project_id])
-    @entry = Entry.new(params[:entry])
+
     @entry.project = @project
-    if @entry.save
+    @entry.description = entry[:description]
+
+    begin
+      date = Date.parse(date)
+      @entry.date = date;
+    rescue
+      @entry.errors.add(:date, 'Invalid date.')
+    end
+
+    begin
+      time = Date.parse(time)
+      @entry.time = time;
+    rescue
+      @entry.errors.add(:time, 'Invalid time.')
+    end
+
+    if @entry.errors
+      render 'projects/show'
+    elsif @entry.save
       redirect back
     else
       render 'projects/show'
