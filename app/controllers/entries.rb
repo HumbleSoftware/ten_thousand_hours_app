15ec3d9db0a755416bf39a7908f7c0b465e9e008
Carl Sutherland <carl@humblesoftware.com>
2011-12-08 19:24:04 -0500

    Implemented entries create.

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -1,6 +1,14 @@
 TenThousandHoursApp.controllers :entries, :parent => :projects do
 
   post :create do
+    @project = Project.get(params[:project_id])
+    @entry = Entry.new(params[:entry])
+    @entry.project = @project
+    if @entry.save
+      redirect back
+    else
+      render 'projects/show'
+    end
   end
 
 end
