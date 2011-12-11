8704b7e2db9215530174b009840dd40be3ffa415
Carl Sutherland <carl@humblesoftware.com>
2011-12-11 17:25:32 -0500

    Validate category.

--- models/entry.rb
+++ models/entry.rb
@@ -6,9 +6,20 @@ class Entry
   validates_presence_of :date
   validates_primitive_type_of :date,
     :message => 'Invalid format.'
+
   validates_presence_of :time
   validates_primitive_type_of :time,
     :message => 'Invalid format.'
+
+  validates_with_block :category_id do
+    if @category_id
+      category = Category.get(@category_id)
+      if !category || category.project_id != @project_id
+        [false, 'invalid category']
+      end
+    end
+    true
+  end
 
   # property <name>, <type>
   property :id, Serial
