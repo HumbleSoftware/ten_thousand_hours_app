5743083cad3b507be69da9edffa192282d258dc3
Carl Sutherland <carl@humblesoftware.com>
2011-12-11 15:09:51 -0500

    Add categories index.

--- app/controllers/categories.rb
+++ app/controllers/categories.rb
@@ -1,5 +1,11 @@
 TenThousandHoursApp.controllers :categories, :parent => :projects do
 
+  get :index do
+    @project = Project.get(params[:project_id])
+    @categories = Category.all(:project_id => @project.id)
+    render 'categories/index'
+  end
+
   post :create do
   end
 
