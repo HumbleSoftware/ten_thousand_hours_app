825284d24414fc37fda9a03fdc4fc1b31c45e24a
Carl Sutherland <carl@humblesoftware.com>
2011-12-11 15:36:34 -0500

    Implemented category creation.

--- app/controllers/categories.rb
+++ app/controllers/categories.rb
@@ -7,6 +7,15 @@ TenThousandHoursApp.controllers :categories, :parent => :projects do
   end
 
   post :create do
+    @project = Project.get(params[:project_id])
+    @category = Category.new(params[:category])
+    @category.project = @project
+
+    if @category.save
+      redirect url(:categories, :index, :project_id => @project.id)
+    else
+      render 'categories/new'
+    end
   end
 
 end
