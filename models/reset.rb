9a235ea3303b436eb191b3aa98a5c06eb28518ce
Carl Sutherland <carl@humblesoftware.com>
2011-12-13 10:38:25 -0500

    Fix default key to be set on _each_ resource.

--- models/reset.rb
+++ models/reset.rb
@@ -4,7 +4,7 @@ class Reset
   # property <name>, <type>
   property :id, Serial
   property :account_id, Integer
-  property :password_key, String, :length => 32, :default => SecureRandom.base64(24)
+  property :password_key, String, :length => 32, :default => lambda { |r, p| SecureRandom.base64(24) }
   property :used, Boolean, :default => true
   property :created_at, DateTime
   property :updated_at, DateTime
