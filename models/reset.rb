7d1de14daff085b75ffb83c6d08c1cee9799ff27
Carl Sutherland <carl@humblesoftware.com>
2011-12-13 10:06:00 -0500

    Added initial validations.

--- models/reset.rb
+++ models/reset.rb
@@ -10,4 +10,8 @@ class Reset
   property :updated_at, DateTime
 
   belongs_to :account
+
+  validates_presence_of :account_id
+  validates_presence_of :password_key
+  validates_presence_of :used
 end
