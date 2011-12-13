c9fcce3017cacb4fc487ef58d2da9d67130a29be
Carl Sutherland <carl@humblesoftware.com>
2011-12-13 10:01:10 -0500

    Added initial spec for reset model.

--- spec/models/reset_spec.rb
+++ spec/models/reset_spec.rb
@@ -5,4 +5,23 @@ describe "Reset Model" do
   it 'can be created' do
     reset.should_not be_nil
   end
+
+  let(:reset) { Reset.new }
+  it 'should set used to true' do
+    reset.valid?
+    reset.used.should be_true
+  end
+
+  let(:reset) { Reset.new }
+  it 'should set 32 character key' do
+    reset.valid?
+    (reset.password_key.length == 32).should be_true
+  end
+
+  let(:reset) { Reset.new }
+  it 'should validate with defaults and user' do
+    reset.account_id = 1
+    reset.valid?.should be_true
+  end
+
 end
