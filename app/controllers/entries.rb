5ad96be300d742b9d615ec1ea44ae1054889d609
Carl Sutherland <carl@humblesoftware.com>
2011-12-15 13:52:54 -0500

    Moved json to vis controller.

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -10,50 +10,10 @@ TenThousandHoursApp.controllers :entries,
     end if protected
   end
 
-  get :index, :provides => [:html, :json] do
-
+  get :index do
     @project = Project.get(params[:project_id])
     @entries = Entry.all(:project_id => @project.id, :order => [:date.asc])
-    @categories = Category.all(:project_id => @project.id)
-
-    case content_type
-      when :json then
-        json = {
-          start_date: @entries.first.date,
-          totals: [],
-          dates: [],
-          descriptions: [],
-          entries: []
-        }
-
-        category_map = {}
-        @categories.each do |c|
-          json[:entries].push label: c.category, data: []
-          category_map[c.id] = category_map.length
-        end
-
-        count = 0
-        total = 0
-        current_date = @entries.first.date
-        json[:dates][count] = current_date
-        @entries.each do |entry|
-          if current_date != entry.date
-            current_date = entry.date
-            json[:totals].push [count, total]
-            json[:dates][count] = current_date.strftime('%m/%d/%Y')
-            count += 1
-          end
-          entries_data = json[:entries][category_map[entry.category_id]][:data]
-          json[:descriptions][entries_data.length] = entry.description
-          entries_data.push [count, entry.time]
-          total += entry.time
-        end
-        json[:totals].push [count, total]
-
-        json.to_json
-      else
-        render 'entries/index'
-      end
+    render 'entries/index'
   end
 
   post :create do
@@ -77,5 +37,4 @@ TenThousandHoursApp.controllers :entries,
       render 'projects/show'
     end
   end
-
 end
--- app/controllers/vis.rb
+++ app/controllers/vis.rb
@@ -7,4 +7,44 @@ TenThousandHoursApp.controllers :vis do
     render 'vis/vis'
   end
 
+  get :data, :with => :project_id, :provides => [:json] do
+
+    @project = Project.get(params[:project_id])
+    @entries = Entry.all(:project_id => @project.id, :order => [:date.asc])
+    @categories = Category.all(:project_id => @project.id)
+
+    json = {
+      start_date: @entries.first.date,
+      totals: [],
+      dates: [],
+      descriptions: [],
+      entries: []
+    }
+
+    category_map = {}
+    @categories.each do |c|
+      json[:entries].push label: c.category, data: []
+      category_map[c.id] = category_map.length
+    end
+
+    count = 0
+    total = 0
+    current_date = @entries.first.date
+    json[:dates][count] = current_date
+    @entries.each do |entry|
+      if current_date != entry.date
+        current_date = entry.date
+        json[:totals].push [count, total]
+        json[:dates][count] = current_date.strftime('%m/%d/%Y')
+        count += 1
+      end
+      entries_data = json[:entries][category_map[entry.category_id]][:data]
+      json[:descriptions][entries_data.length] = entry.description
+      entries_data.push [count, entry.time]
+      total += entry.time
+    end
+    json[:totals].push [count, total]
+
+    json.to_json
+  end
 end
