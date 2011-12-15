7a0a33bf137ebf096d185854709e788173c254ad
Carl Sutherland <carl@humblesoftware.com>
2011-12-14 19:07:52 -0500

    Initial visualization integration.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,20 @@
-// Put your application scripts here
+$(function () {
+
+  var
+    T = TenThousandHours,
+    node = $('.post .vis'),
+    container;
+
+  if (node.length > 0) {
+    container = node[0];
+    $.getJSON('http://devtop:3000/projects/10/entries', function (o) {
+      console.log(container);
+      console.log(o);
+      drawVis(container, o);
+    });
+  }
+
+  function drawVis (container, data) {
+    vis = new T.Vis(node[0], data);
+  }
+});
